5c4e407a3566dfb0c8be5ced6b7c516ccb6a4040
Rishab Bahal <rishabbahal@gmail.com>
2020-07-29 07:56:24 -0400

    2 more tables added on status page

--- controller/status.js
+++ controller/status.js
@@ -14,15 +14,46 @@ exports.getStatus=(req,res,next)=>{
             console.log("Status:")
             console.log(statusArr)
             statusObj={
-                homeRent: statusArr.find(status=>{return status._id.transactionType==='House rent'}).amount,
-                phone: statusArr.find(status=>{return status._id.transactionType==='Phone'}).amount,
-                fuel: statusArr.find(status=>{return status._id.transactionType==='Fuel'}).amount,
-                grocery: statusArr.find(status=>{return status._id.transactionType==='Grocery'}).amount,
-                myGrocery: statusArr.find(status=>{return status._id.transactionType==='MyGrocery'}).amount,
-                others: statusArr.find(status=>{return status._id.transactionType==='Others'}).amount,
-                income: statusArr.find(status=>{return status._id.transactionType==='Income'}).amount
+                homeRent: statusArr.find(status=>{return status._id.transactionType==='House rent'}),
+                phone: statusArr.find(status=>{return status._id.transactionType==='Phone'}),
+                fuel: statusArr.find(status=>{return status._id.transactionType==='Fuel'}),
+                grocery: statusArr.find(status=>{return status._id.transactionType==='Grocery'}),
+                myGrocery: statusArr.find(status=>{return status._id.transactionType==='MyGrocery'}),
+                others: statusArr.find(status=>{return status._id.transactionType==='Others'}),
+                income: statusArr.find(status=>{return status._id.transactionType==='Income'})
             }
-            res.status(200).json({status: statusObj})   
+            Transaction.aggregate([
+                {$match: {date: {$gte:new Date(currentYear,currentMonth,01),$lte:new Date(currentYear,currentMonth,currentDate,23,59)}}},
+                {$group: {_id: {earned:"$earned"},amount:{$sum:"$amount"}}}
+            ])
+            .then(status2Arr=>{
+                status2Obj={
+                    earned: status2Arr.find(status=>{return status._id.earned==='earned'}),
+                    spent: status2Arr.find(status=>{return status._id.earned==='spent'}),
+                    saved: {amount: status2Arr.find(status=>{return status._id.earned==='earned'}).amount-status2Arr.find(status=>{return status._id.earned==='spent'}).amount},
+                }   
+                Transaction.aggregate([
+                    {$match: {date: {$gte:new Date(currentYear,currentMonth,01),$lte:new Date(currentYear,currentMonth,currentDate,23,59)}}},
+                    {$group: {_id: {paymentMethod:"$paymentMethod"},amount:{$sum:"$amount"}}}
+                ])
+                .then(status3Arr=>{
+                    status3Obj={
+                        cibcDebitCard: status3Arr.find(status=>{return status._id.paymentMethod==='CIBC debit card'}),
+                        cibcCreditCard: status3Arr.find(status=>{return status._id.paymentMethod==='CIBC credit card'}),
+                        cash: status3Arr.find(status=>{return status._id.paymentMethod==='Cash'}),
+                        forexCard: status3Arr.find(status=>{return status._id.paymentMethod==='Forex card'}),
+                        interactETransfer: status3Arr.find(status=>{return status._id.paymentMethod==='Interact e transfer'}),
+                        nA: status3Arr.find(status=>{return status._id.paymentMethod==='Not applicable'}),
+                    }
+                    res.status(200).json({status: statusObj,status2: status2Obj,status3: status3Obj})
+                })
+                .catch(err=>{
+                    console.log(err)
+                })
+            })
+            .catch(err=>{
+                console.log(err)
+            })
         }
     )
     .catch(err=>{
