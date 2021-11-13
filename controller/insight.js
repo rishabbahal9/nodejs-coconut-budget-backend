70d19e8671fa5ffde31d51ab79caf44b286b6cdc
Rishab Bahal <rishabbahal@gmail.com>
2021-11-13 14:44:35 -0500

    Basic logic for inst=ight is made.

--- controller/insight.js
+++ controller/insight.js
@@ -1,23 +1,36 @@
 const Transaction = require("../models/transaction");
 
-exports.netIncome = (req, res, next) => {
+const getMonthlyIncomeExpense = (year,month) => {
+  return Transaction.aggregate([
+    {
+      $match: {
+        date: {
+          $gte: new Date(year, month, 01),
+          $lte: new Date(
+            year,
+            month,
+            lastDayOfMonth(year, month),
+            23,
+            59
+          ),
+        },
+      },
+    },
+    { $group: { _id: { earned: "$earned" }, amount: { $sum: "$amount" } } },
+  ])
+};
+const lastDayOfMonth = (y, m) => {
+  return new Date(y, m + 1, 0).getDate();
+};
+exports.netIncome = async (req, res, next) => {
+  
+
   const currentMonth = new Date().getMonth(); // Months are from 0 to 11
-  for (let month = 0; month <= currentMonth; month++) {
-      // Call a method which returns month's total expenses and income
+  const currentYear = new Date().getFullYear(); // Months are from 0 to 11
+  
+  for (let month = 0; month < currentMonth; month++) {
+    const d=await getMonthlyIncomeExpense(currentYear, month)
+    console.log(d);
   }
-  const incomeData = [
-    4000, 4500, 3200, 5000, 2300, 7000, 4000, 5000, 6400, 7200,
-  ];
-  const expenseData = [
-    1250, 2000, 800, 1500, 1300, 2300, 900, 1023, 3002, 2330,
-  ];
-  const netIncomeData = [];
-  for (let x = 0; x < incomeData.length; x++) {
-    netIncomeData[x] = incomeData[x] - expenseData[x];
-  }
-  return res.status(200).json({
-    incomeData: incomeData,
-    expenseData: expenseData,
-    netIncomeData: netIncomeData,
-  });
+  
 };
